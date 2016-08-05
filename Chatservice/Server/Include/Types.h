52489935097a697fca542e68ba9183de9016100a
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2016-08-06 00:56:19 +0200

    Moved Size<> to Types.h

--- Chatservice/Server/Include/Types.h
+++ Chatservice/Server/Include/Types.h
@@ -2,3 +2,14 @@
 
 typedef unsigned char	byte;
 typedef char			sbyte;
+
+// Helper used for determining the total size of template args
+template<typename... Args> struct Size;
+template<> struct Size<>
+{
+	static constexpr size_t value = 0;
+};
+template<typename T, typename... Args> struct Size<T, Args...>
+{
+	static constexpr size_t value = sizeof(T) + Size<Args...>::value;
+};
